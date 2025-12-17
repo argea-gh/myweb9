c1d2f3cacda1a864542da2d6e865c578cc3540dd
argea-gh <argo.armail@gmail.com>
2025-12-17 16:38:40 +0700

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,5 @@
+const products = [
+ {name:'Madu Pahit',cat:'Madu Premium',price:120000,img:'https://hni.net/public/front/img/produk/MADU%20PAHIT-1_04-01-19_.png',badge:'Favorit Pelanggan'},
+ {name:'Centella Teh Sinergi',cat:'Minuman Sehat',price:70000,img:'https://hni.net/public/front/img/produk/CENTELLA-1_04-01-19_.png',badge:'Cocok untuk Pemula'},
+ {name:'Deep Olive',cat:'Minyak Herba',price:145000,img:'https://hni.net/public/front/img/produk/deep-olive-0625_16-06-25_.png',badge:'Unggulan Kami'},
+];
